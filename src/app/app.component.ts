6206dfd77f486a187d420cb41b0ff34874c7a922
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2024-09-06 17:50:41 -0400

    custom board to see webcam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideoPlayerComponent } from "./video-player/video-player.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VideoPlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angularproctoring';
+export class AppComponent implements OnInit{
+  public currentStream: any;
+  ngOnInit(): void {
+    this.checkMediaSource();
+  }
+  checkMediaSource = () => {
+    if (navigator && navigator.mediaDevices) {
+      navigator.mediaDevices.getUserMedia(  {
+        audio: false,
+        video: true
+      }).then(stream =>{
+        this.currentStream = stream;
+      }).catch(()=>{
+        console.log("no hay permisos");
+
+      })
+    }else{
+      console.log("No se encontro dispositivos ");
+
+    }
+  }
 }
